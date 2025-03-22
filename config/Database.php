0b76af338b8fb5f611d434c7ff9eaf1167692ec4
Amanda Lyons <avatar.mandu@gmail.com>
2025-03-22 11:56:07 -0700

    Updated Database.php and added .gitignore

--- config/Database.php
+++ config/Database.php
@@ -1,34 +1,32 @@
 <?php 
 class Database {
+  private $conn;
   private $host;
+  private $port;
   private $dbname;
   private $username;
   private $password;
-  private $conn;
 
   public function __construct() {
-    $this->host = getenv('DB_HOST'); // Remove the localhost fallback
-    $this->port = '5432';
-    $this->dbname = getenv('DB_DATABASE');
-    $this->username = getenv('DB_USERNAME');
-    $this->password = getenv('DB_PASSWORD');
-}
-
+    $this->username = getenv('USERNAME'); // Fixed key name
+    $this->password = getenv('PASSWORD');
+    $this->dbname = getenv('DBNAME');
+    $this->host = getenv('HOST');
+    $this->port = getenv('PORT');
+  }
 
   public function connect() {
-    $this->conn = null;
-
-    try {
-      $this->conn = new PDO(
-        "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname}",
-        $this->username,
-        $this->password
-      );
-      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e) {
-      echo 'Connection Error: ' . $e->getMessage();
+    if ($this->conn) {
+      return $this->conn;
+    } else {
+      $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
+      try {
+        $this->conn = new PDO($dsn, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Fixed typo
+        return $this->conn;
+      } catch (PDOException $e) {
+        echo 'Connection Error: ' . $e->getMessage();
+      }
     }
-
-    return $this->conn;
   }
 }
